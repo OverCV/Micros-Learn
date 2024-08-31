eb10d2e3b70b8f1ac36354b7d60bc056c1ef3012
OverCV <over.castrillon22396@ucaldas.edu.co>
2024-08-31 09:09:20 -0500

    Updated for ATMega 2650.

---
+++ projects/Nibbles/Nibbles.asm
@@ -0,0 +1,38 @@
+    .include"m2560def.inc"
+
+    .org(0x0000)
+    RJMP main
+
+main:
+    LDI     R16,        0xA5
+    STS     0x0200,     R16
+    LDS     R17,        0x0200
+
+    ; RJMP    lower_nibble
+    ; RJMP    all_nibbles
+    RJMP    any_nibble
+
+lower_nibble:
+    ; Hacer que el nibble bajo de la memoria posición 200H valga F
+    ORI     R17,        0x0F
+    STS     0x0200,     R17
+
+    RJMP    lower_nibble
+
+all_nibbles:
+    ; Hacer que todo el nibble valga 0 (posición 200H)
+    ANDI    R17,        0x00
+    STS     0x0200,     R17
+
+    RJMP    all_nibbles
+
+any_nibble:
+    ; Hacer que algún nibble se niegue (posición 200H)
+    LDI     R18,        0x0F    ; Negar parte baja
+    LDI     R19,        0xF0    ; Negar parte alta
+
+    EOR     R17,        R19
+    STS     0x0200,     R17
+
+    RJMP    any_nibble
+
