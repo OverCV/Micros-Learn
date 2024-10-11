97923dcd13ba229b305481fcc948e2e4e541bef7
OH <over.castrillon22396@ucaldas.edu.co>
2024-10-11 11:19:58 -0500

    Movements with timer, blink is now functionall.

--- projects/TimMoves/TimMoves.asm
+++ projects/TimMoves/TimMoves.asm
@@ -37,11 +37,16 @@ main:
     LDI     R20,            0       ; Contador de interrupciones
     LDI     R18,            64      ; 64 interrupciones para contar 1 segundo
 
-
+    ; Inicializar vector de estados
+    LDI     R25,            0x00
 
 loop:
+    ; LDI     R20,            0x00
+    IN      R21,            PIND
+    ANDI    R21,            0x03
     OUT     PORTA,          R20     ; Mostrar el valor del contador en el Puerto A
     RJMP    loop                    ; Loop infinito
+
 
     ; Rutina de interrupción para Timer0 Compare Match A
 isr_t0:
@@ -50,7 +55,36 @@ isr_t0:
 
     ; Si se alcanzaron 64 interrupciones, incrementar contador de segundos
     LDI     R18,            64      ; Reiniciar el contador de interrupciones
+
+    ; LOGIC
     INC     R20                     ; Incrementar el contador de segundos
+
+    LDI     R20,            0x00    ; Reseteo de la pantalla
+    CPI     R21,            0x01
+    ; mover el valor en la subrutina, acá limpiarlo, allá recuperarlo???
+    BREQ    blink
 
 reti_isr_t0:
     RETI                            ; Retornar de la interrupción
+
+blink:
+    SBRC    R25,            0       ; ..0 -> on
+    COM     R20
+
+    ; XOR b0R23
+    LDI     R19,            0x01
+    EOR     R25,            R19
+
+    RJMP    reti_isr_t0
+
+
+
+    /*
+    ; R25: 0000 0000
+
+    ; b0: Blink
+    ; R25.0 = on, R25.1 = off
+
+    ; b1: 
+
+    */
