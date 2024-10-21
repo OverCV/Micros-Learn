165c048f1456d8ff539e1feaac796c3909c52fb9
OH <over.castrillon22396@ucaldas.edu.co>
2024-10-21 02:00:43 -0500

    Refactoring files.

--- tests/Zero/Zero.asm
+++ tests/Zero/Zero.asm
@@ -26,101 +26,103 @@
 
 main:
     ; Definición entradas o salidas
-    LDI     R16,        0xFF
-    OUT     DDRA,       R16
-    OUT     DDRC,       R16
+    LDI     R16,            0xFF
+    OUT     DDRA,           R16
+    OUT     DDRC,           R16
 
     ; Vector de estado
-    LDI     R25,        0x00
+    LDI     R25,            0x00
 
     ; Interrupciones globales
     SEI
 
 timer_clock_conf:
     ; Configurar Timer0 en modo CTC (Clear Timer on Compare Match)
-    LDI     R16,        0x02                        ; WGM01 = 1, modo CTC
-    OUT     TCCR0A,     R16
+    LDI     R16,            0x02                        ; WGM01 = 1, modo CTC
+    OUT     TCCR0A,         R16
     ; Establecer el valor máximo en OCR0A
-    LDI     R16,        ONE_SEC
-    OUT     OCR0A,      R16                         ; Valor de comparación en OCR0A
+    LDI     R16,            ONE_SEC
+    OUT     OCR0A,          R16                         ; Valor de comparación en OCR0A
     ; Configurar el prescaler en 1024 (CS02 = 1, CS01 = 0, CS00 = 1)
-    LDI     R16,        0x05
-    OUT     TCCR0B,     R16
+    LDI     R16,            0x05
+    OUT     TCCR0B,         R16
     ; Habilitar la interrupción de comparación para el Timer0 (OCIE0A)
-    LDI     R16,        0x02                        ; Habilitar interrupción de comparación (OCIE0A)
-    STS     TIMSK0,     R16
+    LDI     R16,            0x02                        ; Habilitar interrupción de comparación (OCIE0A)
+    STS     TIMSK0,         R16
     ; Inicializar el contador de interrupciones
-    LDI     R17,        ONE_SEC
-    LDI     R19,        0
+    LDI     R17,            ONE_SEC
+    LDI     R19,            0
 
 interruptions_conf:
     ; Activar INT(0, 1)
-    LDI     R16,        0x03
-    OUT     EIMSK,      R16
+    LDI     R16,            0x03
+    OUT     EIMSK,          R16
     ; Configurar detección de cambio
-    LDI     R16,        0x0F
-    STS     EICRA,      R16
+    LDI     R16,            0x0F
+    STS     EICRA,          R16
 
 reset_table:
-    SBRC    R25,        1
+    SBRC    R25,            1
     RJMP    reset_y
 
 reset_x:
-    LDI     ZH,         HIGH(table_name    *  2)
-    LDI     ZL,         LOW(table_name     *  2)
+    LDI     ZH,             HIGH(table_name    *  2)
+    LDI     ZL,             LOW(table_name     *  2)
 
-    LDI     R16,        8
-    SBRC    R25,        0   ; b0=0 inicia
-    ADD     ZL,         R16
+    LDI     R16,            8
+    SBRC    R25,            0                           ; b0=0 inicia
+    ADD     ZL,             R16
     RJMP    loop
 
 reset_y:
-    LDI     ZH,         HIGH(table_nums    *  2)
-    LDI     ZL,         LOW(table_nums     *  2)
-    LDI     R16,        4
-    SBRC    R25,        0   ; b0=0 inicia
-    ADD     ZL,         R16
+    LDI     ZH,             HIGH(table_nums    *  2)
+    LDI     ZL,             LOW(table_nums     *  2)
+    LDI     R16,            4
+    SBRC    R25,            0                           ; b0=0 inicia
+    ADD     ZL,             R16
     RJMP    loop
 
 loop:
-    LPM     R20,        Z+
-    LPM     R21,        Z
+    ; Mantenemos estable la lectura en Z
+    LPM     R20,            Z+
+    LPM     R21,            Z
     DEC     ZL
 
     ; comparar si se salió y consultar vector de estado
-    SBRC    R21,        7
+    SBRC    R21,            7
     RJMP    reset_table
 
+    ; Guardado en tabla para Cátodo Común
     COM     R20
     COM     R21
 
-    OUT     PORTA,      R20
-    OUT     PORTC,      R21
+    OUT     PORTA,          R20
+    OUT     PORTC,          R21
 
     RJMP    loop
 
 isr_0:
-    LDI     R16,    0x01
-    EOR     R25,    R16
+    LDI     R16,            0x01
+    EOR     R25,            R16
     RETI
 
 isr_1:
-    LDI     R16,    0x02
-    EOR     R25,    R16
+    LDI     R16,            0x02
+    EOR     R25,            R16
     RETI
 
 tisr_0:
     DEC     R17
     BRNE    tisr_reti
-    LDI     R17,        ONE_SEC
+    LDI     R17,            ONE_SEC
 
-    LDI     R16,        2
+    LDI     R16,            2
 
-    SBRC    R25,        0
-    SUB     ZL,         R16
+    SBRC    R25,            0
+    SUB     ZL,             R16
 
-    SBRS    R25,        0
-    ADD     ZL,         R16
+    SBRS    R25,            0
+    ADD     ZL,             R16
 
 tisr_reti:
     RETI
@@ -132,11 +134,4 @@ table_name:
     .org        (0x0200)
 table_nums:
     .dw         0x220f, 0x0006
-    ; .dw         0x003f, 0x221B, 0x220f, 0x2227, 0x223d
-
-    ; En este programa se almacenará el código y cédula de un estudiante en la memoria Flash del programa y se mostrará por un conflay 7Segmentos ánodo común.
-    ; Según una entrada de 02 bits se mostrará el código (0) o cédula (1) con el bit menos significativo, el más significativo mostrará los dígitos en orden ascendente (0) o descendente (1).
-
-    ; 00: ASC Código. 01: ASC Cédula. 10: DESC Código. 11: DESC Cédula ;
-
-    ; Se usarán los puertos B0 y B1 para posterior implementación con interrupciones.
+    ; .dw         0x003f, 0x221B, 0x220f, 0x2227, 0x223D
