7385de6f53fb7d279289dbc2d6a8b58055826603
OH <over.castrillon22396@ucaldas.edu.co>
2025-01-09 00:35:39 -0500

    Addition of docs

--- 2560 core/asm content/1-Mirror/Mirror.asm
+++ 2560 core/asm content/1-Mirror/Mirror.asm
@@ -2,17 +2,37 @@
     .org(0x0000)
     RJMP    main
 
+    ; Programa donde la entrada del bit respectivo al puerto A enciende la salida del bit asociado del puerto C.
     ; Los pines de entrada en puerto D desde 0 -> 3
     ; Serán vistos como salidas del puerto D desde 4 -> 7
-main:
-    LDI     R16,    0x00    ;   Salidas 7->4, Entradas 0->3
-    OUT     DDRA,   R16
 
-    LDI     R16,    0xFF    ;   Salidas 7->4, Entradas 0->3
-    OUT     DDRC,   R16
+    ; Data directions para configurar
+    ; Pines son para lectura
+    ; Puertos son para escritura
+
+main:
+    ; DDRx = Data Direction Register x - Dirección de x
+    LDI     R16,    0x00    ; 0000 0000
+    OUT     DDRA,   R16     ; CONF DDRA, ENTRADA
+
+    LDI     R16,    0xFF    ; 1111 1111
+    OUT     DDRC,   R16     ; CONF DDRC, SALIDA
 
 loop:
-    IN      R16,    PINA
-    OUT     PORTC,  R16
+    IN      R16,    PINA    ; [0000 0000]
+    OUT     PORTC,  R16     ; ESCRIBA, PORTC, R16
 
+    ; Banderas: OVF CARR NEG .. ZER
+    ; evaluación de la condición -> R16 == 0 -> Alterar la banderas
+    ; BREQ main
+    ; BRanch if EQual
+
+
+    ; Relative JuMP
     RJMP    loop
+
+0x0000 00 00 BF AF 00 00 00
+0x0001 00 00 00 00 00 00 00
+0x0002 00 00 00 00 00 00 00
+...
+0x000F 00 00 00 00 00 00 00
