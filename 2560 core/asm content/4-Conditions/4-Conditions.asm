3cac046f2842762ebe9355a2f250cb2651343038
OH <over.castrillon22396@ucaldas.edu.co>
2025-01-13 08:21:29 -0500

    Addition of conds explain.

--- 2560 core/asm content/4-Conditions/4-Conditions.asm
+++ 2560 core/asm content/4-Conditions/4-Conditions.asm
@@ -28,13 +28,13 @@ loop:
     ANDI    R17,    0x0F     
     ANDI    R18,    0x0F     
 
-    CPI     R19,    0
+    CPI     R19,    0       ; 0 = 0b0000 0001 = 0x01
     BREQ    sum
 
-    CPI     R19,    1
+    CPI     R19,    1       ; 2 = 0b0000 0002 = 0x02
     BREQ    subs
 
-    CPI     R19,    2
+    CPI     R19,    2       ; 3 = 0b0000 0003 = 0x03
     BREQ    prod
 
     RJMP    half
@@ -58,6 +58,6 @@ prod:
 
 half:
     LSR     R20
-    MOV     R17,    R20    
+    MOV     R17,    R20
     RJMP    loop
 
