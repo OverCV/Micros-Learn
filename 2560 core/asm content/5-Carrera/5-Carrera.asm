b806dde9ff85ca77910aeb5e09ed7d4dbb2dcc19
OH <over.castrillon22396@ucaldas.edu.co>
2025-01-23 15:37:43 -0500

    Semi explaied.

--- 2560 core/asm content/5-Carrera/5-Carrera.asm
+++ 2560 core/asm content/5-Carrera/5-Carrera.asm
@@ -23,11 +23,9 @@ main:
     LDI     R16,    0x7F
     OUT     DDRC,   R16
 
-    ; Estado del led
-    ; 01, 02, 40, 20, 10, 08, 04, 40, 20
     LDI     R20,    0x01
-    LDI     DEL1,   100
-    LDI     DEL2,   255
+    LDI     DEL1,   255
+    LDI     DEL2,   50
     LDI     DEL3,   255
 
 reset:
@@ -47,24 +45,39 @@ loop:
     
     ; CPI     R17,    0x02    ; 0000 0010
     ; BREQ    ocho
-    RJMP    ocho
+    RJMP    ocho_ez
 
 cero:
     RJMP    loop
-    
-ocho:
+
+ocho_ez:
+    ; Estado del led
+    ; 01, 02, 40, 20, 10, 08, 04, 40, 20
+    ; A > B > G > E > D > C > G > F
+
+    ; __GFE.DCBA
+    ; __654.3210
+
+    ; _____.___A
+    ; __000.0001
     LDI     R20,    0x01
     OUT     PORTC,  R20
     RCALL   delay
 
+    ; _____.__B_
+    ; __000.0010
     LDI     R20,    0x02
     OUT     PORTC,  R20
     RCALL   delay
 
+    ; __G__.____
+    ; __100.0000
     LDI     R20,    0x40
     OUT     PORTC,  R20
     RCALL   delay
 
+    ; ____E.____
+    ; __001.0000
     LDI     R20,    0x10
     OUT     PORTC,  R20
     RCALL   delay
@@ -92,9 +105,9 @@ delay:
     BRNE    delay
 
     DEC     DEL2
-    LDI     DEL1,   100
     BRNE    delay
 
     DEC     DEL3
+    LDI     DEL2,   50
     BRNE    delay
     RET
