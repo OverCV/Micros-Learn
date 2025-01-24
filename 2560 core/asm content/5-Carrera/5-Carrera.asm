69d47de540fa0340f04622cca5b8a53f6d24a483
OH <over.castrillon22396@ucaldas.edu.co>
2025-01-24 17:18:57 -0500

    Addition of sobroutines explanation

--- 2560 core/asm content/5-Carrera/5-Carrera.asm
+++ 2560 core/asm content/5-Carrera/5-Carrera.asm
@@ -10,9 +10,9 @@
 01: Vuelta en ocho
 */
 
-.def DEL1 = R25
-.def DEL2 = R26
-.def DEL3 = R27
+.def CNT1 = R25
+.def CNT2 = R26
+.def CNT3 = R27
 
 main:
     ; Definir entradas
@@ -24,9 +24,9 @@ main:
     OUT     DDRC,   R16
 
     LDI     R20,    0x01
-    LDI     DEL1,   255
-    LDI     DEL2,   50
-    LDI     DEL3,   255
+    LDI     CNT1,   255
+    LDI     CNT2,   50
+    LDI     CNT3,   255
 
 reset:
 
@@ -126,14 +126,99 @@ ocho_ez:
 
     RJMP    loop
 
+
+
+; Registro de subrutina actual
+
+; [0 0 0 0 . 0 0 0 0]
+; subrutina_x:
+;     LDI     R30     1
+;     ...
+;     RJMP    delay   
+
+
+; subrutina_y:
+;     LDI     R30     2
+;     ; ...
+;     RJMP    delay   
+
+; subrutina_z:
+;     LDI     R30     3
+;     ...
+;     RJMP    delay   
+
+
 delay:
-    DEC     DEL1
+    DEC     CNT1
     BRNE    delay
 
-    DEC     DEL2
+    DEC     CNT2
     BRNE    delay
 
-    DEC     DEL3
-    LDI     DEL2,   50
+    DEC     CNT3
+    LDI     CNT2,   50
     BRNE    delay
-    RET
+
+    ; SBRS    R30,    0
+    ; RJMP    subrutina_x
+
+    ; SBRS    R30,    0
+    ; RJMP    subrutina_x
+
+    RJMP    loop
+
+
+    ; RET
+; subrutina_x:
+;     LDI R25,    5
+;     ...
+;     ; cargue R20 con FF
+;     ; mostré R20 por PORTC
+;     ; Subritina de retraso
+;     RCALL   delay
+
+; algo_recursivo:
+;     LDI R25,    6
+;     ...
+;     RCALL   delay
+;     algo_rec_2:
+;     LDI R25,    7
+
+;     ...
+
+    
+;     CPI     R25,    5
+;     BREQ    subrutina_x
+
+;     CPI     R25,    6
+;     BREQ    algo_rec_2
+
+;     RJMP   loop
+
+
+/*
+reset:
+    R20 = 0000.0000
+    R21 = 0000.0001
+    R22 = 0001.1000
+
+loop:
+    if x => parpadeo
+
+    ; if y => der_izq
+
+    RJMP    loop
+
+parpadeo:
+    R20 = 0000.0000 -> 1111.1111
+
+der_izq:
+    R21 = 0000.0001 -> 0000.0010
+          0000.0010 -> 0000.0100
+          0000.0010 -> 0000.1000
+
+expandir:
+    R22 = 0001.1000 -> 0010.0100
+    RJMP    loop
+*/
+
